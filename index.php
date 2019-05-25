a64e8755c21f3400ecc229ccd80a369e1b87bae1
Areso <returnstrike@yandex.ru>
2019-05-25 21:12:05 +0500

    added gitignore, added head to index.php

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
 <?php
-echo "Hello world!";
-
+if (is_file('config.php')) {
+        require_once('config.php');
+}
+//echo 'result is '.$DB_USERNAME;
+//$subnet = getenv("REMOTE_ADDR");!!!!!!!!!
+//set charset
+ini_set("default_charset",'utf-8');//utf-8 windows-1251
+ini_set('display_errors', 1);
+error_reporting('E_ALL & E_STRICT');
 ?>
