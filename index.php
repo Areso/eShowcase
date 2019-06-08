685698a9ca36f38cb3f7ecc809f897702aad96aa
Areso <returnstrike@yandex.ru>
2019-06-08 16:58:17 +0600

    unblock the first button

--- index.php
+++ index.php
@@ -124,8 +124,8 @@ echo "<body>";
 //echo "</div>";
 
 echo "<div class='tab'>";
-echo "<button class='tablinks' onclick='openTab(event,'Main')' id='tabCity'>Main</button>";
-//echo "<button class='tablinks' onclick='openTab(event, 'Explore')' id='tabExplore'>Explore</button>";
+echo "<button class='tablinks' onclick='openTab(event,'Main')'    id='tabCity'   >Main   </button>";
+echo "<button class='tablinks' onclick='openTab(event,'Explore')' id='tabExplore'>Explore</button>";
 echo "</div>";
 echo "<div id='Main' class='tabcontent'>";
 echo "</div>";
