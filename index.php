b1a89f498317a2b30f1ec5e13b02cbaefc1d735f
Areso <returnstrike@yandex.ru>
2019-06-08 23:48:19 +0600

    one another way

--- index.php
+++ index.php
@@ -124,7 +124,8 @@ echo "<body>";
 //echo "</div>";
 
 echo "<div class='tab'>";
-echo "<button id='tabCity'    class='tablinks' onclick="."'openTab(event,'"."Main')'>Main   </button>";
+<button id='tabCity'    class='tablinks' onclick='openTab(event,'Main')'>Main   </button>
+echo $a;
 //onclick='openTab(event,'Main')' 
 echo "<button id='tabExplore' class='tablinks' >Explore</button>";
 //onclick='openTab(event,'Explore')' 
