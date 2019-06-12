e20806fcf35e59e35850ecc184f174593e625487
Areso <returnstrike@yandex.ru>
2019-06-13 01:40:53 +0500

    categories done; promo done

--- index.php
+++ index.php
@@ -24,61 +24,9 @@ if (!mysqli_set_charset($conn, "utf8")) {
     //printf("Current character set: %s\n", mysqli_character_set_name($conn));
 }
 
-$query_line = "SELECT
- *
- FROM goods
- WHERE id_group = 1";
-
-$query = mysqli_query($conn, $query_line) or die("Query error while checking goods: ".mysqli_connect_error($conn).mysqli_errno($conn).mysqli_error($conn));
-$field = mysqli_field_count($conn);
-
-$skus     = [];
-$names    = [];
-$groups   = [];
-$photos   = [];
-$descs    = [];
-$prices   = [];
-$qtys     = [];
-
-
-while($row = mysqli_fetch_array($query)) {
-	for($i = 0; $i < $field; $i++) {
-		if ($i==1) {
-			$sku = $row[mysqli_fetch_field_direct($query, $i)->name];
-			array_push($skus, $sku);
-		}
-		if ($i==2) {
-              $item_name = $row[mysqli_fetch_field_direct($query, $i)->name];
-              array_push($names, $item_name);
-        }
-        if ($i==3) {
-              $group_name = $row[mysqli_fetch_field_direct($query, $i)->name];
-              array_push($groups, $group_name);
-        }
-        if ($i==4) {
-              $photo = $row[mysqli_fetch_field_direct($query, $i)->name];
-              array_push($photos, $photo);
-        }
-        if ($i==5) {
-              $desc = $row[mysqli_fetch_field_direct($query, $i)->name];
-              array_push($descs, $desc);
-        }
-        if ($i==6) {
-              $price = $row[mysqli_fetch_field_direct($query, $i)->name];
-              array_push($prices, $price);
-        }
-        if ($i==7) {
-              $qty = $row[mysqli_fetch_field_direct($query, $i)->name];
-              array_push($qtys, $qty);
-        }
-	}
-}
-$goodies_len = sizeof($skus);
-
 $query_line_cat = "select * from groups";
 $query_cat = mysqli_query($conn, $query_line_cat) or die("Query error while checking categories: ".mysqli_connect_error($conn).mysqli_errno($conn).mysqli_error($conn));
 $field_cat = mysqli_field_count($conn);
-echo "field number is ".$field_cat;//DEBUG
 $id_groups      = [];
 $name_groups    = [];
 
@@ -91,13 +39,13 @@ while($row = mysqli_fetch_array($query_cat)) {
         }
         if ($i==1) {
             $name_group = $row[mysqli_fetch_field_direct($query_cat, $i)->name];
-            echo "name group is ".$name_group; //DEBUG!
             array_push($name_groups, $name_group);
         }
 	}
 }
 $cat_len = sizeof($id_groups);
-echo "the size of categories is ".$cat_len." and cat0 is ".$name_groups[0];
+//echo "the size of categories is ".$cat_len;
+
 echo "<html>";
 echo "<head>";
 echo "<title>";
@@ -120,76 +68,141 @@ echo "<body>";
 echo "<div id='page'>";
 //echo "<div id='logo'>logotype.png</div>";
 //echo "<div id='myheader'>eShop</div>";
-//echo "<div id='menu' class='menu'>menu";
-//echo "<div class='menuone'>group 1</div>";
-//echo "<br>";
-//echo "<div class='menuone'>group 2</div>";
-//echo "</div>";
+
 
 echo "<div class='tab'>";
 echo "<button id='tabCity'      class='tablinks' onclick=\"openTab(event, 'Main')\"       >Главная</button>";
-echo "<button id='tabCategory1' class='tablinks' onclick=\"openTab(event, 'Category1')\"  >$name_groups[0]</button>";
+for($i = 0; $i < $cat_len; $i++) {
+	$newCat = $i+1;
+
+	$query_line_show = "select * from goods where id_group = $newCat";
+	$query_show = mysqli_query($conn, $query_line_show) or die("Query error while checking number of goods in categories: ".mysqli_connect_error($conn).mysqli_errno($conn).mysqli_error($conn));
+	if (mysqli_num_rows($query_show)>0) {
+		echo "<button id='tabCategory$newCat' class='tablinks' onclick=\"openTab(event, 'Category$newCat')\"  >$name_groups[$i]</button>";
+	}
+}
 echo "</div>";
 echo "<div id='Main' class='tabcontent'>";
-echo "Пожалуйста, выберите категорию товаров в меню сверху!";
-echo "</div>";
-echo "<div id='Category1' class='tabcontent'>";
-	echo "<div class='goods'>";
-		echo "<table border='1'>";
-		echo "<tr>";
-		echo "<td>SKU</td>";
-		echo "<td>Название</td>";
-		//echo "<td>Код производителя</td>";
-		echo "<td>Фото</td>";
-		echo "<td>Описание</td>";
-		echo "<td>Цена</td>";
-		echo "<td>Количество</td>";
-		echo "<td>Кол-во к покупке</td>";
-		echo "</tr>";
-		for ($i=0; $i<$goodies_len; $i++) {
-			echo "<tr>";
-
-			echo "<td>";
-			echo $skus[$i];
-			echo "</td>";
-
-			echo "<td>";
-			echo $names[$i];
-			echo "</td>";
-
-			//echo "<td>";
-			//echo $groups[$i];
-			//echo "</td>";
-
-			echo "<td>";
-			echo "<img src='images/$groups[$i]/$photos[$i]'></img>";
-			echo "</td>";
-
-			echo "<td>";
-			echo $descs[$i];
-			echo "</td>";
-
-			echo "<td>";
-			echo $prices[$i];
-			echo "</td>";
-
-			echo "<td>";
-			echo $qtys[$i];
-			echo "</td>";
-
-			echo "<td>";
-			echo "<input type='text' id='input".$skus[$i]."' name='qtyToBuy' size='5' value='0'>";
-			echo "</tr>";
-		}
-		echo "</table>";
-	echo "<button onclick='makeOrder()'>Сформировать заказ</button>";
-	echo "<br>";
-	echo "<br>";
-	echo "</div>";
-echo "</div>";
-
-
+echo "Пожалуйста, выберите категорию товаров в меню сверху!<br>";
 echo "ИП Пермина К.И. ИНН 667116868763 ОГРНИП 315665800056070";
+echo "</div>";
+
+for($y = 0; $y < $cat_len+1; $y++) {
+	//echo "current category is $y <br>";
+	$query_line = "SELECT
+	 *
+	 FROM goods
+	 WHERE id_group = $y";
+
+	$query = mysqli_query($conn, $query_line) or die("Query error while checking goods: ".mysqli_connect_error($conn).mysqli_errno($conn).mysqli_error($conn));
+	$field = mysqli_field_count($conn);
+
+	$skus     = [];
+	$names    = [];
+	$groups   = [];
+	$photos   = [];
+	$descs    = [];
+	$prices   = [];
+	$qtys     = [];
+
+
+	while($row = mysqli_fetch_array($query)) {
+		for($i = 0; $i < $field; $i++) {
+			if ($i==1) {
+				$sku = $row[mysqli_fetch_field_direct($query, $i)->name];
+				array_push($skus, $sku);
+			}
+			if ($i==2) {
+				  $item_name = $row[mysqli_fetch_field_direct($query, $i)->name];
+				  array_push($names, $item_name);
+			}
+			if ($i==3) {
+				  $group_name = $row[mysqli_fetch_field_direct($query, $i)->name];
+				  array_push($groups, $group_name);
+			}
+			if ($i==4) {
+				  $photo = $row[mysqli_fetch_field_direct($query, $i)->name];
+				  array_push($photos, $photo);
+			}
+			if ($i==5) {
+				  $desc = $row[mysqli_fetch_field_direct($query, $i)->name];
+				  array_push($descs, $desc);
+			}
+			if ($i==6) {
+				  $price = $row[mysqli_fetch_field_direct($query, $i)->name];
+				  array_push($prices, $price);
+			}
+			if ($i==7) {
+				  $qty = $row[mysqli_fetch_field_direct($query, $i)->name];
+				  array_push($qtys, $qty);
+			}
+		}
+	}
+	$goodies_len = sizeof($skus);
+	//echo "goods number in category $y is ".$goodies_len."<br>";
+	if ($goodies_len > 0) { 
+		echo "<div id='Category$y' class='tabcontent'>";
+		echo "<div class='goods'>";
+			echo "<table border='1'>";
+			echo "<tr>";
+			echo "<td>Артикул</td>";
+			echo "<td>Название</td>";
+			echo "<td>Фото</td>";
+			echo "<td>Описание</td>";
+			echo "<td>Цена</td>";
+			echo "<td>Количество</td>";
+			echo "<td>Кол-во к покупке</td>";
+			echo "</tr>";
+			for ($z=0; $z<$goodies_len; $z++) {
+				echo "<tr>";
+
+				echo "<td>";
+				echo $skus[$z];
+				echo "</td>";
+
+				echo "<td>";
+				echo $names[$z];
+				echo "</td>";
+
+				echo "<td>";
+				echo "<img src='images/$groups[$y]/$photos[$z]'></img>";
+				echo "</td>";
+
+				echo "<td>";
+				echo $descs[$z];
+				echo "</td>";
+
+				echo "<td>";
+				echo $prices[$z];
+				echo "</td>";
+
+				echo "<td>";
+				echo $qtys[$z];
+				echo "</td>";
+
+				echo "<td>";
+				echo "<input type='text' id='input".$skus[$z]."' name='qtyToBuy' size='5' value='0'>";
+				echo "</tr>";
+			}
+			echo "</table>";
+		echo "<button onclick='makeOrder()'>Сформировать заказ</button>";
+		echo "<br>";
+		echo "<br>";
+		// file_exists ( string $filename ) : bool
+		echo "<a href='images/$groups[$y]/promo.zip'>скачать промоматериалы</a>";
+		echo "<br>";
+		echo "<br>";
+		echo "<br>";
+		echo "ИП Пермина К.И. ИНН 667116868763 ОГРНИП 315665800056070";
+		echo "</div>";
+		echo "</div>";
+	} else {
+		//!
+	}
+}
+	
+
+
 echo "</div>";
 echo "</body>";
 echo "</html>";
