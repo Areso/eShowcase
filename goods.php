fa6aef2e3ca133d74b6537a4e3521def37849ee9
Anton Gladyshev <returnstrike@yandex.ru>
2019-09-29 22:47:11 +0300

    locking goods page is done

--- goods.php
+++ goods.php
@@ -1,7 +1,7 @@
 <?php
 	include('site-backend/lock.php');
+	include('site-backend/goods-lock.php');
 	$myAuth   = include 'site-backend/menualterer.php';
-	$myVerify = include 'site-backend/verify_check.php';
 	$myVers   = include 'site-backend/versionsite.php';
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
